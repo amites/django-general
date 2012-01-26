4e1859975c0abad09cf6019dca1145f3d86213e2
Alvin Mites <alvin@mitesdesign.com>
2012-01-26 14:25:03 -0700

    Minor updates to model structure

--- general/models/__init__.py
+++ general/models/__init__.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-
 class DefaultModel(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
@@ -8,3 +7,14 @@ class DefaultModel(models.Model):
 
     class Meta:
         abstract=True
+
+class ZipCode(models.Model):
+    zip = models.CharField(unique=True, max_length=15)
+    state = models.CharField(max_length=6)
+    state_name = models.CharField(max_length=50, blank=True)
+    latitude = models.CharField(max_length=30)
+    longitude = models.CharField(max_length=30)
+    city = models.CharField(max_length=150, blank=True)
+
+    class Meta:
+        db_table = u'zip_codes'
