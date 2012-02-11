345f3564d9c838254b59de51a547ecd7c2d89d63
Alvin Mites <alvin@mitesdesign.com>
2012-02-10 21:32:41 -0700

    Added fields

---
+++ general/fields.py
@@ -0,0 +1,6 @@
+#from django.contrib.auth.models import User
+from django.forms import ModelChoiceField
+
+class UserModelChoiceField(ModelChoiceField):
+    def label_from_instance(self, obj):
+        return obj.get_full_name()
