82a5264a3962d0a817add7ad4db783c7a378f62f
Alvin Mites <alvin@mitesdesign.com>
2012-05-15 14:49:24 -0600

    bug fix

--- general/models/slugged.py
+++ general/models/slugged.py
@@ -4,20 +4,20 @@ from general.unique_slugify import unique_slugify
 
 
 class Slugged(models.Model):
-    slug = models.SlugField()
-
-    try:
-        name = slug.name
-    except AttributeError:
-        try:
-            name = self.name
-        except AttributeError:
-            raise Exception('Model %s must have field `name`' \
-                + 'or define `slug_name`' % self._meta.)
+    slug = models.SlugField(unique=True)
 
     def save(self, *args, **kwargs):
         if self.slug == '':
-            self.slug = unique_slugify(self, self.name)
+            try:
+                name = self.slug_name
+            except AttributeError:
+                try:
+                    name = self.name
+                except AttributeError:
+                    raise Exception('Model %s must have field `name`' \
+                        + 'or define `slug_name`' % self._meta.object_name)
+            self.slug = unique_slugify(self, name)
+
         super(Slugged, self).save(*args, **kwargs)
 
     class Meta:
